3d559d366c2e5476b0be05c19769e598c3a95354
Mahif9900 <mkaif2304@gmail.com>
2026-03-13 12:33:24 +0530

    Integrated puzzle generator with streak system

--- src/utils/main.js
+++ src/utils/main.js
@@ -1,5 +1,6 @@
 import { generateDailyPuzzle, validanswer, validateAnswer } from "./puzzleGenerator.js"
 import { startTimer, stopTimer } from "./timer.js"
+import { updateStreak } from "./streakSystem.js";
  
 const puzzle = generateDailyPuzzle();
 startTimer();
@@ -9,6 +10,23 @@ if(correct) {
     const timeTaken = stopTimer()
     console.log("Correct!")
     console.log("Solved in", timeTaken, "seconds")
+    let progress = JSON.parse(localStorage.getItem("progress")) || {
+    currentStreak:0,
+    longestStreak:0,
+    puzzlesSolved:0,
+    lastSolvedDate:null
+    }
+
+    // update streak
+    progress = updateStreak(progress)
+
+    // save updated progress
+    localStorage.setItem("progress", JSON.stringify(progress))
+
+    console.log("Current Streak:", progress.currentStreak)
+    console.log("Longest Streak:", progress.longestStreak)
+    console.log("Puzzles Solved:", progress.puzzlesSolved)
+
 }else {
     console.log("wrong answer, try again tomorrow!")
 }
